a7aa8f0c50e5a043b3596f1c7021b8ef17a34dd3
elliotfiske <elliotfiske@gmail.com>
2016-01-26 02:22:21 -0800

    Blinn phuuuuung

--- keyframe_asgn/resources/simple_frag.glsl
+++ keyframe_asgn/resources/simple_frag.glsl
@@ -1,11 +1,19 @@
-#version 120
-
-varying vec3 fragNor;
+uniform vec3 UaColor;
+uniform vec3 UdColor;
+varying vec3 vH;
+varying vec3 vNor;
+varying vec3 vLight;
+varying vec3 vCol;
 
 void main()
 {
-	vec3 normal = normalize(fragNor);
-	// Map normal in the range [-1, 1] to color in range [0, 1];
-	vec3 color = 0.5*normal + 0.5;
-	gl_FragColor = vec4(color, 1.0);
+   vec3 newH = normalize(vH);
+   vec3 newNor = normalize(vNor);
+   vec3 newLight = normalize(vLight);
+   
+   vec3 lDiffuseColor = vec3(1.0, 1.0, 1.0) * max(0.0, dot(normalize(newNor), normalize(newLight))) * UdColor;
+   vec3 lAmbientColor = vec3(1.0, 1.0, 1.0) * UaColor;
+   vec3 lSpecularColor = vec3(1.0, 1.0, 1.0) * UdColor * max(0.0, dot(normalize(newNor), normalize(newH)));
+   gl_FragColor = vec4(lDiffuseColor + lAmbientColor + lSpecularColor, 1.0);
+   
 }
