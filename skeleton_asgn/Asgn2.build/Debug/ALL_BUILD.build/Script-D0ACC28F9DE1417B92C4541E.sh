897e972725dd9510f636fc567b93b17c2159bca1
elliotfiske <elliotfiske@gmail.com>
2016-02-10 00:03:25 -0800

    Thanks for playing

---
+++ skeleton_asgn/Asgn2.build/Debug/ALL_BUILD.build/Script-D0ACC28F9DE1417B92C4541E.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/elliotfiske/animation/skeleton_asgn -f /Users/elliotfiske/animation/skeleton_asgn/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ skeleton_asgn/CMakeFiles/3.2.2/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/i386/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/elliotfiske/animation/skeleton_asgn/CMakeFiles/3.2.2/CompilerIdCXX/CMakeCXXCompilerId.cpp
